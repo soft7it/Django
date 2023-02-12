8f58a5e5486784bcfee3e297c170b27bdb65e6d2
VITALIE 777 <soft7.it@gmail.com>
2023-02-12 13:51:11 +0000

    function_tax

---
+++ tax_app/tax.py
@@ -0,0 +1,15 @@
+from os import system
+
+#### calculate tax_income  #############
+
+def calculateTax():
+    amount      # suma de bani
+    interes     # dobinda procente
+    target_borrow  # salariu , imprumut
+    tax_salarry
+    pass
+
+#######  clear #########################
+
+def clear():
+    system("cls")  
