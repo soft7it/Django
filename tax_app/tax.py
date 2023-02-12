d2ac05d28bfbfc6e159d193c347c3e679430c73d
VITALIE 777 <soft7.it@gmail.com>
2023-02-12 14:28:09 +0000

    update_data_tax_examples

--- tax_app/tax.py
+++ tax_app/tax.py
@@ -2,12 +2,14 @@ from os import system
 
 #### calculate tax_income  #############
 
-def calculateTax():
-    amount      # suma de bani
-    interes     # dobinda procente
-    target_borrow  # salariu , imprumut
-    tax_salarry
-    pass
+def calculateTax(amount):
+    #amount      # suma de bani
+    interes = 0.02     # 2% dobinda procente
+    comission_interes = amount * interes
+    # target_borrow  # salariu , imprumut
+    tax_income = 0.2 # 20%
+    tax_salary = amount * tax_income
+    return comission_interes, 'for rate credit. ', tax_salary, 'for income tax.'
 
 #######  clear #########################
 
