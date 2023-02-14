80c2120b36d5e8fd43bc38c4ab6e5870ca8bd2d1
VITALIE 777 <soft7.it@gmail.com>
2023-02-14 16:21:13 +0000

    Create sing_in.py

---
+++ singIn/sing_in.py
@@ -0,0 +1,27 @@
+"""
+The idea behind this homework is for you to imagine how does the SIGNUP function work in a social network
+Create a function called signUp()
++ It should take 3 parameters: username, email, password
+It should verify that the all 3 are strings
+It should verify that the username has at least 5 and at most 12 characters and consists only of latin (a..zA..Z) letters and digits (0..9)
+It should verify the validity of the email address (@, .)
+It should require a password of at least 8 characters long
+It should return a dictionary that contains these three values
+It should raise exceptions when something does not validate
+"""
+from logic_singin import *
+
+while True:
+
+    clear()
+
+    username = input('Username nust be no less 5 or more 12 caracters : ')
+    # email = input('Email : ')
+    # password = input('password : ')
+
+    print(singUp(username)) # , email, password
+
+    query = input("Would you like check again (y/n) ?  ")
+
+    if query != 'y':
+        break
