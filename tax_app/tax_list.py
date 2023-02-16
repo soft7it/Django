f376fdfe9b130dd27fdea163c6e9b248eb3a729e
VITALIE 777 <soft7.it@gmail.com>
2023-02-16 13:29:12 +0000

    function_apend_list

--- tax_app/tax_list.py
+++ tax_app/tax_list.py
@@ -2,20 +2,20 @@ from os import system
 
 #### calculate tax_income  #############
 
-def calculateTax(amount):
+def calculateTax(amount, interes, scopul_sumei):
 
     total_dict = [  ]   # create a list
      
     total_dict.append( amount )
+    total_dict.append( interes )
     total_dict.append( amount * 0.12 )# 12%
-    # total_dict["sum"] = amount
-
-    # print(total_dict)
-    return total_dict
-
+    total_dict.append( scopul_sumei )
     
+    return total_dict
+    
+    # return [amount, interes, amount * (interes / 100), scopul_sumei] # folosim ca lista
 
 #######  clear #########################
 
 def clear():
-    system("cls")  
+    system("cls")   
