971acbf0cd87920dd6c61e1ff74feafb10cd2dfe
VITALIE 777 <soft7.it@gmail.com>
2023-02-16 13:15:55 +0000

    function_return_dict_list_tuple

--- tax_app/tax.py
+++ tax_app/tax.py
@@ -2,20 +2,28 @@ from os import system
 
 #### calculate tax_income  #############
 
-def calculateTax(amount):
+def calculateTax(amount, interes, scopul_sumei):
 
-    total_dict = {    } # create a dictionar
+    # # total_dict = {    } # create a dictionar
      
-    total_dict["suma"] = amount
-    total_dict["interes"] = amount * 0.12 # 12%
-    # total_dict["sum"] = amount
+    # # total_dict["suma"] = amount
+    # # total_dict["interes"] = interes
+    # # total_dict["interes_amount"] = amount * (interes / 100) # 12%    
+    # # total_dict["scopul_sum"] = scopul_sumei
+
+    total_dict = {"suma": amount,
+                  "interes": interes, 
+                  "interes_amount" : amount * (interes / 100),
+                  "scopul_sum": scopul_sumei}  # create a dictionar
 
     # print(total_dict)
     return total_dict
 
-    
+   # return (amount, interes, amount * (interes / 100), scopul_sumei) # folosim ca tuple
+
+   # return [amount, interes, amount * (interes / 100), scopul_sumei] # folosim ca lista    
 
 #######  clear #########################
 
 def clear():
-    system("cls")  
+    system("cls")    
