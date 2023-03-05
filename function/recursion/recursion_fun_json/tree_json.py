094513d754f62149dff45a1c2319c8a1b69f66b8
VITALIE 777 <soft7.it@gmail.com>
2023-03-05 18:42:06 +0000

    tree_json_func_recursion_for_in

---
+++ function/recursion/recursion_fun_json/tree_json.py
@@ -0,0 +1,42 @@
+tree = [
+    {
+    'id' : 'A',
+    'children' : [
+            {
+                'id' : 'B',
+                'children' : [
+                    {
+                        'id' : 'D',
+                        'children' : []
+                    },
+                ]            
+            },
+            {
+                'id': 'C',
+                'children': [
+                    {
+                        'id': 'E',
+                        'children': []
+                    },
+                    {
+                        'id': 'H',
+                        'children': [
+                            {
+                                'id': 'G',
+                                'children': []
+                            },
+                            {
+                                'id': 'I',
+                                'children': []
+                            },
+                        ]
+                    },
+                    {
+                        'id': 'F',
+                        'children': []
+                    },
+                ]
+            },
+            ]
+    }
+]
