83c29dafde153e4572dec928cae0cc44549266ed
VITALIE 777 <soft7.it@gmail.com>
2023-03-16 00:05:03 +0000

    patern_loose_coupling

--- patern/factory/product.py
+++ patern/factory/product.py
@@ -1,12 +1,30 @@
 # Module Defines the 'Product' class
 # Provider
-
+from time import sleep
 # Class = Creator + Structure + Behavior
 
-class Prpduct:
+class _Food:                        # this is a private class
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
     def __str__(self) -> str:
-        return f'[Product]<{self.name:15} : {self.price:7.2f}>'
+        return f'[Food]<{self.name:15} : {self.price:7.2f}>'
+
+class _Drink:                        # this is a private class
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self) -> str:
+        return f'[Drink]<{self.name:15} : {self.price:7.2f}>'
+
+# factory function
+def createProduct(type, name, price):  # loose coupling
+    sleep(2)  # 2 secunde asteapta 
+    if type == 'food':
+        return _Food(name, price)
+    elif type == 'drink':
+        return _Drink(name, price)
+    else:
+        raise TypeError('only "food" and "drink" products can be fabricated!')
