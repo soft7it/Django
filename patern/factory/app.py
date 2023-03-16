25a1496224165369776373faa9820e85cddf6a43
VITALIE 777 <soft7.it@gmail.com>
2023-03-16 00:03:49 +0000

    patern_app

--- patern/factory/app.py
+++ patern/factory/app.py
@@ -1,8 +1,11 @@
 # Main Module : Consumer
 from product import *
 
-p1 = Prpduct('Salad', 100.00)  # __init__()
-p2 = Prpduct('Soup', 50.00)
+# p1 = Prpduct('Salad', 100.00)  # __init__()
+# p2 = Prpduct('Grape Juice', 50.00)
+
+p1 = createProduct('food', 'Salad', 100.00)  # __init__()
+p2 = createProduct('drink', 'Grape Juice', 50.00)
 
 print(p1)  # str(p1)
 print(p2)  # str(p2)
