28b5d6269a8a345536b1a510340fd10f40dcce2b
VITALIE 777 <soft7.it@gmail.com>
2023-03-25 14:09:04 +0000

    done_HW

--- patern/obsever_patern.py
+++ patern/obsever_patern.py
@@ -1,13 +1,14 @@
 from time import sleep
 
 class CentralBankPublisher:
-    def __init__(self):
+    def __init__(self,):
           # Bank Role
           self.__rates = {
                'EUR_USD': None,
                'USD_EUR': None,
           }
-          # oBSERVER ROLE
+          # OBSERVER ROLE
+        #   self.__subscribers = ["Construction Materials Store", "Auto Service 'Victory'"]
           self.__subscribers = []
 
     def subscribe(self, subscriber):
@@ -23,9 +24,9 @@ class CentralBankPublisher:
 
     def setRate(self, form_currency, to_currency, rate):
         # HW1: use  form_currency, to_currency, -> to set the value
-
+        self.__rates[f'{form_currency}_{to_currency}'] = rate
         ## !!! CODE THAT NOTIFIES !!!
-        self.__rates['EUR_USD'] = rate
+        # self.__rates['EUR_USD'] = rate
         #                       ^
         #                      HERE
         self.notifySubscribers()
@@ -47,13 +48,12 @@ class Subscriber:
     
     def update(self):
         print(f'{self.name} COT THE NOTIFICATION !')
-
 ##################################################################################
-
 cb = CentralBankPublisher()
 cb.subscribe(Subscriber('Construction Materials Store'))
 cb.subscribe(Subscriber('Auto Service \'Victory\''))
+# cb.unsubscribe('Auto Service \'Victory\'')  # unsubsribe from the list self.__subscribers = []
 cb.setRate('EUR', 'USD', 1.2)
-sleep(2) #timer 2sec.
-cb.setRate('EUR', 'USD', 1.1)
+sleep(2)    #timer 2sec.
+cb.setRate('USD', 'EUR', 1.1)
 print(cb)        
