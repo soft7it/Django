044071aad57f498a2157088e7489f1986ec3e24d
VITALIE 777 <soft7.it@gmail.com>
2023-04-06 16:15:22 +0100

    Update app.py

--- Domain-Driven Design( DDD )/app.py
+++ Domain-Driven Design( DDD )/app.py
@@ -1,5 +1,7 @@
 from domain.EtentyFactory import EntityFactory
 
 u1 = EntityFactory.create('user',{'username': 'John', 'email': 'j@d.com', 'password': '123456'})
+u2 = EntityFactory.create('post', {'title': 'good', 'body':'great', 'authorId':' Romaska'})
 
 print(u1)
+print(u2)
