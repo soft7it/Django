9de899fd817f83d1d522522c3ed6de1beff30106
VITALIE 777 <soft7.it@gmail.com>
2023-04-08 21:37:08 +0100

    Update EtentyFactory.py

--- Domain-Driven Design( DDD )/domain/EtentyFactory.py
+++ Domain-Driven Design( DDD )/domain/EtentyFactory.py
@@ -3,37 +3,41 @@ from time import time
 
 from .User import User
 from .Post import Post
-from .Comment import Comment 
-from .Message import Message
-from .Reaction import Reaction  
+from .IdentificationService import IdentificationService
+# from .Comment import Comment 
+# from .Message import Message
+# from .Reaction import Reaction  
 
 class EntityFactory:
 
     def create(type, params):  # params - dict
         if type == 'user':
             user = User(params['username'], params['email'], params['password'],)
-            user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+            # user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+            user.id = IdentificationService.getId()
             return user
         ######################################
         elif type == 'post':
             post = Post(params['title'], params['body'], params['authorId'],)
-            post.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+            post.id = IdentificationService.getId()
             return post
-        ###################################### 
-        elif type == 'comment':
-            comment = Comment(params['id'], params['body'], params['targetId'], params['authorId'],)
-            comment.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-            return comment
-        ########################################
-        elif type == 'message':
-            message = Message(params['id'], params['type'], params['targetId'], params['authorId'],)
-            message.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-            return message
-        ########################################
-        elif type == 'reaction':
-            reaction = Reaction(params['id'], params['type'], params['targetId'], params['authorId'],)
-            reaction.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-            return reaction
-        
+                
         else:
             raise TypeError(f'EntityFactory cannot create {type} type !!!')
+        
+        
+        ###################################### 
+        # elif type == 'comment':
+        #     comment = Comment(params['id'], params['body'], params['targetId'], params['authorId'],)
+        #     comment.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+        #     return comment
+        # ########################################
+        # elif type == 'message':
+        #     message = Message(params['id'], params['type'], params['targetId'], params['authorId'],)
+        #     message.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+        #     return message
+        # ########################################
+        # elif type == 'reaction':
+        #     reaction = Reaction(params['id'], params['type'], params['targetId'], params['authorId'],)
+        #     reaction.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+        #     return reaction
