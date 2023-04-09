21829818c78bf401770b55d4d74710d6352421b8
VITALIE 777 <soft7.it@gmail.com>
2023-04-09 22:14:25 +0100

    Update app.py

--- Domain-Driven Design( DDD )/app.py
+++ Domain-Driven Design( DDD )/app.py
@@ -1,7 +1,16 @@
 from domain.EtentyFactory import EntityFactory
+from domain.UserRepository import UserRepository
 
 u1 = EntityFactory.create('user',{'username': 'John', 'email': 'j@d.com', 'password': '123456'})
-u2 = EntityFactory.create('post', {'title': 'good', 'body':'great', 'authorId':' Romaska'})
+UserRepository.saveUser(u1)
+## save in DB
+u1.username = 'new name'
+## update in DB
+UserRepository.updateUser(u1)
 
-print(u1)
-print(u2)
+users = UserRepository.getAllUsers()
+# print(users)
+
+for u in users:
+    print(u.id)
+    print(u.username)
