fc4602a8ff6070bcc27a69e14068b745b74b12c2
VITALIE 777 <soft7.it@gmail.com>
2023-04-09 22:13:36 +0100

    Update EtentyFactory.py

--- Domain-Driven Design( DDD )/domain/EtentyFactory.py
+++ Domain-Driven Design( DDD )/domain/EtentyFactory.py
@@ -10,34 +10,19 @@ from .IdentificationService import IdentificationService
 
 class EntityFactory:
 
-    def create(type, params):  # params - dict
+    def create(type, params, withId = True):  # params - dict
         if type == 'user':
             user = User(params['username'], params['email'], params['password'],)
-            # user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-            user.id = IdentificationService.getId()
+            if withId:
+                # user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
+                user.id = IdentificationService.getId()
             return user
         ######################################
         elif type == 'post':
             post = Post(params['title'], params['body'], params['authorId'],)
-            post.id = IdentificationService.getId()
+            if withId:
+                post.id = IdentificationService.getId()
             return post
                 
         else:
             raise TypeError(f'EntityFactory cannot create {type} type !!!')
-        
-        
-        ###################################### 
-        # elif type == 'comment':
-        #     comment = Comment(params['id'], params['body'], params['targetId'], params['authorId'],)
-        #     comment.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-        #     return comment
-        # ########################################
-        # elif type == 'message':
-        #     message = Message(params['id'], params['type'], params['targetId'], params['authorId'],)
-        #     message.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-        #     return message
-        # ########################################
-        # elif type == 'reaction':
-        #     reaction = Reaction(params['id'], params['type'], params['targetId'], params['authorId'],)
-        #     reaction.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
-        #     return reaction
