738dcd3d1ff18fbad226cbb20786140866ff36a8
VITALIE 777 <soft7.it@gmail.com>
2023-04-14 16:54:19 +0100

    Update app.py

--- Domain-Driven Design( DDD )/app.py
+++ Domain-Driven Design( DDD )/app.py
@@ -14,10 +14,12 @@ for u in users:
     print(u.id)
     print(u.username)
 print('*'*50)
+
 #######  getObj ############################
 print(users)
 
 print('*'*50)
+
 #######  getUser() ############################
 u1 = UserRepository.getUser('56987')
 if u1:
@@ -25,15 +27,18 @@ if u1:
 else:
     print('User not found.')
 print('*'*50)
+
 #######  deleteUser() ############################
-u1 = UserRepository.getUser('56987')
-UserRepository.deleteUser(u1)
-if u1:
-    print(UserRepository.db)
-else:
-    print('User not found.')
-print('*'*50)
-#######  postUser() ############################
+# u1 = UserRepository.getUser('56987')
+# UserRepository.deleteUser(u1)
+# if u1:
+#     print(UserRepository.db)
+# else:
+#     print('User not found.')
+# print('*'*50)
+
+#######  postUser() 1 ############################
+print('FIRST POST')
 u1 = UserRepository.getUser('12345')
 
 p1 = EntityFactory.create('post',{
@@ -45,8 +50,67 @@ p1 = EntityFactory.create('post',{
 UserRepository.savePost(p1)
 print(UserRepository.db)
 print('*'*50)
+#######  postUser() 2 ############################
+print('FIRST POST')
+u1 = UserRepository.getUser('56987')
+
+p1 = EntityFactory.create('post',{
+    'title': 'Hello friends',
+    'body' : 'learning software',
+    'authorId' : u1.id
+})
+
+UserRepository.savePost(p1)
+print(UserRepository.db)
+print('*'*50)
+
+#######  updatePost() 1 ############################
+print('UpDate POST ')
+u1 = UserRepository.getUser('12345')
+
+p1.title = 'The UpDate the first post by jhony'
+p1.body = 'xaxaxaxa xaxxaxa xaxaxxaaaxa'
+
+UserRepository.updatePost(p1)
+print(UserRepository.db)
+print('*'*50)
+#######  updatePost() 2 ############################
+print('UpDate POST ')
+u1 = UserRepository.getUser('56987')
+
+p1.title = 'Meau zoro'
+p1.body = 'IHAaxaxa xaxxaxa xaxaxxaaaxa'
+
+UserRepository.updatePost(p1)
+print(UserRepository.db)
+print('*'*50)
+
+#######  getPost()    ############################
+print('GET POST 1')
+user_post = UserRepository.getPost('12345')
+if user_post:
+    print(user_post)
+else:
+    print('Post not found.')
+print('*'*50)
+
+#######  getAllPost() ############################
+print('GET ALL POSTS')
+posts = UserRepository.getAllPosts()
+
+for u in posts:
+    print('*'*25)
+    print(u.id)
+    print(u.title)
+    print(u.body)
+print('*'*50)
 
 #######  deletePost() ############################
-#######  updatePost() ############################
-#######  getPost()    ############################
-#######  getAllPost() ############################
+print('DELETE')
+u1 = UserRepository.getUser('12345')
+UserRepository.deletePost(u1)
+if u1:
+    print(UserRepository.db)
+else:
+    print('Post not found.')
+print('*'*50)
