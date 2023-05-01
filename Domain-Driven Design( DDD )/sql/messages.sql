1b6b0f7a1c2ac16b2edc54e97067e27b6fbd74a0
VITALIE 777 <soft7.it@gmail.com>
2023-05-01 23:37:34 +0100

    Update messages.sql

--- Domain-Driven Design( DDD )/sql/messages.sql
+++ Domain-Driven Design( DDD )/sql/messages.sql
@@ -1,8 +1,8 @@
 CREATE TABLE messages (
     id uuid DEFAULT uuid_generate_v4()
+    'type' VARCHAR NOT NULL
 
     PRIMARY KEY (id),
     FOREIGN KEY (author_id) REFERENCES users(id)
-    FOREIGN KEY (post_id) REFERENCES(post_id)
-    
+    FOREIGN KEY (post_id) REFERENCES(post_id)    
 );
