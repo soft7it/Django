daa82182b86899e85fc4fe1fd5482442b3506853
VITALIE 777 <soft7.it@gmail.com>
2023-05-01 23:36:27 +0100

    Update EtentyFactory.py

--- Domain-Driven Design( DDD )/domain/EtentyFactory.py
+++ Domain-Driven Design( DDD )/domain/EtentyFactory.py
@@ -3,10 +3,11 @@ from time import time
 
 from .User import User
 from .Post import Post
+from .Comment import Comment
+from .Reaction import Reaction
+from .Message import Message
 from .IdentificationService import IdentificationService
-# from .Comment import Comment 
-# from .Message import Message
-# from .Reaction import Reaction  
+# user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
 
 class EntityFactory:
 
@@ -14,7 +15,6 @@ class EntityFactory:
         if type == 'user':
             user = User(params['username'], params['email'], params['password'],)
             if withId:
-                # user.id = f'{int(time() * 1000)}-{randint(100000000, 9000000000)}'
                 user.id = IdentificationService.getId()
             return user
         ######################################
@@ -23,6 +23,28 @@ class EntityFactory:
             if withId:
                 post.id = IdentificationService.getId()
             return post
+                
+        ######################################
+        elif type == 'comment':
+            comment = Comment(
+                params['type'], params['authorId'], params['post_id'])
+            if withId:
+                comment.id = IdentificationService.getId()
+            return comment
+                       
+        ######################################
+        elif type == 'reaction':
+            reaction = Reaction(params['type'], params['authorId'], params['post_id'])
+            if withId:
+                reaction.id = IdentificationService.getId()
+            return reaction
+                
+        ######################################
+        elif type == 'message':
+            message = Message(params['body'], params['authorId'], params['receiverId'])
+            if withId:
+                message.id = IdentificationService.getId()
+            return message
                 
         else:
             raise TypeError(f'EntityFactory cannot create {type} type !!!')
