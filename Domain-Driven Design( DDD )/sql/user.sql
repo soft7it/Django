a504cbda8efa2744566429048352eaf0ed2f55c3
VITALIE 777 <soft7.it@gmail.com>
2023-05-17 23:57:21 +0100

    Update user.sql

--- Domain-Driven Design( DDD )/sql/user.sql
+++ Domain-Driven Design( DDD )/sql/user.sql
@@ -2,11 +2,11 @@ CREATE TABLE users (
     id uuid DEFAULT uuid_generate_v4 (),
     username VARCHAR NOT NULL,
     email VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 
-INSERT INTO users(username, email, pasword) VALUES( 'Miki Mouse', 'mouse@t.com', '123456')
-INSERT INTO users(username, email, pasword) VALUES( 'Pokemon Filimon', 'pokemon@p.com', '123456')
+INSERT INTO users(username, email, "password") VALUES( 'Miki Mouse', 'mouse@t.com', '123456')
+INSERT INTO users(username, email, "password") VALUES( 'Pokemon Filimon', 'pokemon@p.com', '123456')
