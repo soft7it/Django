8892e810f6ddcd9344eaa4039bf66451b4fd86f7
VITALIE 777 <soft7.it@gmail.com>
2023-05-18 23:55:04 +0100

    Update reactions.sql

--- Domain-Driven Design( DDD )/sql/reactions.sql
+++ Domain-Driven Design( DDD )/sql/reactions.sql
@@ -1,9 +1,18 @@
 CREATE TABLE reactions (
-    id uuid DEFAULT uuid_generate_v4()
-    'type' VARCHAR NOT NULL
+    id uuid DEFAULT uuid_generate_v4(),
+    "Type" VARCHAR NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES users(id)
-    FOREIGN KEY (post_id) REFERENCES(post_id)
+    FOREIGN KEY (author_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id)
     
 );
+
+-- Miki Mouse posted first reaction
+INSERT INTO reactions(title, body, author_id, post_id)
+VALUES ('ha-ha-ha', 'eef06d38-d565-4185-8dd5-973acbd6a51e', '5bf66dfd-5b3a-4c67-a407-9024aaff095b');
+
+
+-- Pokemon Filimon posted first reaction
+INSERT INTO reactions(title, body, author_id, post_id)
+VALUES ('Mea-uuuuuu', '80222429-67d6-425b-b0e9-71312e79a16c', '2652b1bb-c3ef-47c8-9dab-3ee6505bec68');
