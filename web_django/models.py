bcc1bac3c331122c4c1fd75f6176d498c44cdb26
VITALIE 777 <soft7.it@gmail.com>
2023-08-08 23:48:38 +0100

    Update models.py

--- web_django/models.py
+++ web_django/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+
+# proxy Extending model
+class CustomUser(User):
+    avatar = models.CharField(max_length=150, default='')  
+
+    # Friendship!!!
+    friends = models.ManyToManyField('self')
+    session_data_backup = models.TextField(default='')    
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +17,11 @@ class Post(models.Model):
     comments = models.CharField(max_length=200, default='')
     messages = models.CharField(max_length=200, default='')
     posts = models.CharField(max_length=200, default='')
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Comment(models.Model):
+    pass
